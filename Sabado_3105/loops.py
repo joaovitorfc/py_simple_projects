5d51a9bdeaa88bb2ff59d6a22158f245ff9a0311
unknown <freiresjv@gmail.com>
2025-06-01 15:20:20 -0300

    Aprendendo Laços de Repetição: For (2.0)

--- Sabado_3105/loops.py
+++ Sabado_3105/loops.py
@@ -9,6 +9,48 @@
     #print("terminou")
  
 criancas = ['joao','maria','ana']
+            #0,1,2
+#for item in criancas: # item é o negocio ponha como um indice.
+    #print(item)
 
-for item in criancas: # item é o negocio ponha como um indice.
-    print(item)
+
+#canal = "refatorando"
+
+#for letra in canal:
+    #print(letra)
+
+# o LEN ve o tamanho
+#for index in range(len(criancas)): #O terceiro é a quantidade que ele vai pular, no caso tá pulando de 2 em 2.
+ #   print(criancas[index],index)
+
+#for index in range(5):  # o 0 é a primeira linha.
+ #   if index == 0:
+  #      print("primeia linha")
+   # else:
+    #    print(f"outras linhas {index}")  
+
+matriz_numeros=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0],
+]
+
+for linha in matriz_numeros:
+  #  print(linha)
+    print("----")
+    for coluna in linha:
+        print(coluna)
+
+
+
+
+
+
+
+
+
+
+
+
+
