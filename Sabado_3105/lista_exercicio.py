1fedfe6583af27ff13af4b7329b35155d55b430e
unknown <freiresjv@gmail.com>
2025-06-01 14:11:07 -0300

    Aprendendo While e For: Laços de Reepetição

---
+++ Sabado_3105/lista_exercicio.py
@@ -0,0 +1,31 @@
+# Exercício:
+# 1. Crie uma lista com os números de 1 a 10.
+# 2. Adicione o número 11 ao final da lista.
+# 3. Remova o número 5.
+# 4. Inverta a ordem da lista.
+# 5. Exiba a lista final.
+
+
+numeros=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numeros.append(11) # Adiciona um Valor a lista.
+numeros.remove(5)
+numeros.reverse()
+print(f"o valor final da lista é: {numeros}" )
+
+
+
+
+
+
+#Cálculo da Soma:
+#Crie uma lista com os números de 1 a 10.
+#Calcule a soma de todos os números na lista.
+#Exiba o resultado.
+
+numeros = [1, 2, 3, 4, 5,6,7,8,9,10]
+soma = sum(numeros)
+print("A soma dos elementos é:", soma)
+
+# Uso de While
+
---
+++ Sabado_3105/loops.py
@@ -0,0 +1,14 @@
+# Uso de While
+
+#i = 1
+
+#while i < 10:
+    #print(i)
+    #i + 1
+
+    #print("terminou")
+ 
+criancas = ['joao','maria','ana']
+
+for item in criancas: # item é o negocio ponha como um indice.
+    print(item)
